4883c20f05a6feb7647b20ba422da7206dbbfe1c
Donald KANTI <donald.kanti@mehriso.com>
2026-02-13 13:27:03 +0100

    chore: remove tracked node_modules and dist

--- node_modules/slidev-theme-penguin/setup/main.ts
+++
@@ -1,6 +0,0 @@
-import { AsGraphic } from '@alvarosabu/ui'
-import { defineAppSetup } from '@slidev/types'
-
-export default defineAppSetup(({ app }) => {
-  app.component('AsGraphic', AsGraphic)
-})
--- node_modules/slidev-theme-penguin/setup/shiki.ts
+++
@@ -1,7 +0,0 @@
-/* ./setup/shiki.ts */
-import { defineShikiSetup } from '@slidev/types'
-import PenguinTheme from './theme/penguin-theme.json'
-
-export default defineShikiSetup(() => ({
-  theme: PenguinTheme,
-}))
